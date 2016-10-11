784ca78170fbfd1a8fdacf4cab99ce7ffcc293ce
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-11 18:02:40 -0400

    Setup for follow-along of examples in Chap. 5 - Working with Razor

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
