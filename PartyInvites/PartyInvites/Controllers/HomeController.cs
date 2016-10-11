dff093621b1bcd6e312f3ca5d1ff640c23a6d7d4
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-10 20:04:24 -0400

    Finished 'PartyInvites' project, as per chapter 2

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() => View();
+
+        [HttpPost]
+        // TODO: Email response to the party organizer
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", guestResponse);
+            else
+                return View();
+        } 
+    }
+}
