c2d36ac6654515e592c92cdeaeaa359afbf40276
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-12 11:09:45 -0400

    Initial steps in Ninject.

--- EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -1,24 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using EssentialTools.Models;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
         private Product[] products = {
             new Product { Name = "Kayak", Price = 275M },
             new Product { Name = "Lifejacket", Price = 48.95M },
             new Product { Name = "Soccer ball", Price = 19.50M },
             new Product { Name = "Corner Flag", Price = 34.95M }};
 
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
-            LinqValueCalculator calc = new LinqValueCalculator();
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
 
             decimal totalValue = cart.CalculateProductTotal();
---
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,24 @@
+﻿using Ninject;
+using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using EssentialTools.Models;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) => kernel.TryGet(serviceType);
+        public IEnumerable<object> GetServices(Type serviceType) => kernel.GetAll(serviceType);
+
+        private void AddBindings() => kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+    }
+}
