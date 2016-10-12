170bd7c6353a073a092f4748b0761aa2f97714fd
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-12 10:24:28 -0400

    Essentials for Chap. 6

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product { Name = "Kayak", Price = 275M },
+            new Product { Name = "Lifejacket", Price = 48.95M },
+            new Product { Name = "Soccer ball", Price = 19.50M },
+            new Product { Name = "Corner Flag", Price = 34.95M }};
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+    }
+}
---
+++ EssentialTools/EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal() => calc.ValueProducts(Products);
+    }
+}
