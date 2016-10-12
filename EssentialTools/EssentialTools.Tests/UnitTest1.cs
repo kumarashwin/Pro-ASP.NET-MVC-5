8eb0accfa01dcb48175f5216672f555a9a490c24
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-12 16:02:50 -0400

    Finished Chap. 6 - Reviewed how to do Unit Tests and learned how to use Moq

---
+++ EssentialTools/EssentialTools.Tests/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+
+namespace EssentialTools.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IDiscountHelper getTestObject() => new MinimumDiscountHelper();
+
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            //Arrange
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            //Act
+            var discountedTotal = target.ApplyDiscount(total);
+
+            //Assert
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+            
+        }
+
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            //Arrange
+            IDiscountHelper target = getTestObject();
+
+            //Act
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            //Assert
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 discount is wrong");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 discount is wrong");
+        }
+
+        [TestMethod]
+        public void Discount_Less_Than_10()
+        {
+            //Arrange
+            IDiscountHelper target = getTestObject();
+
+            //Act
+            decimal discount5 = target.ApplyDiscount(5);
+            decimal discount0 = target.ApplyDiscount(0);
+
+            //Assert
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Discount_Negative_Total()
+        {
+            //Arrange
+            IDiscountHelper target = getTestObject();
+
+            //Act
+            target.ApplyDiscount(-1);
+
+            //Assert
+            
+        }
+    }
+}
---
+++ EssentialTools/EssentialTools.Tests/UnitTest2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+using System.Linq;
+using Moq;
+
+namespace EssentialTools.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products = {
+            new Product { Name = "Kayak", Price = 275M },
+            new Product { Name = "Lifejacket", Price = 48.95M },
+            new Product { Name = "Soccer ball", Price = 19.50M },
+            new Product { Name = "Corner Flag", Price = 34.95M }};
+
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            //Arrange
+            var mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+
+            //Act
+            var result = target.ValueProducts(products);
+
+            //Assert
+            Assert.AreEqual(products.Sum(e => e.Price), result);
+        }
+
+        private Product[] createProduct(decimal value) => new[] { new Product { Price = value } };
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            //Arrange
+            var mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0))).Throws<System.ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100))).Returns<decimal>(total => total * 0.9M);
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive))).Returns<decimal>(total => total - 5);
+
+            var target = new LinqValueCalculator(mock.Object);
+
+            //Act
+            decimal FiveDollarDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(createProduct(10)); ;
+            decimal FiftyDollarDiscount = target.ValueProducts(createProduct(50)); ;
+            decimal HundredDollarDiscount = target.ValueProducts(createProduct(100)); ;
+            decimal FiveHundredDollarDiscount = target.ValueProducts(createProduct(500)); ;
+
+            //Assert
+            Assert.AreEqual(5, FiveDollarDiscount, "$5 Fail");
+            Assert.AreEqual(5, TenDollarDiscount, "$10 Fail");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 Fail");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 Fail");
+            Assert.AreEqual(450, FiveHundredDollarDiscount, "$500 Fail");
+            target.ValueProducts(createProduct(0));
+        }
+    }
+}
