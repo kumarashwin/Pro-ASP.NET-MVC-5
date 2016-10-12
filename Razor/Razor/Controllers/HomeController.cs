acdeb128840f4b924ca884492e357b83e57ab578
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-11 20:07:27 -0400

    Finished Chap. 5 - Working with Razor

--- Razor/Razor/Controllers/HomeController.cs
+++ Razor/Razor/Controllers/HomeController.cs
@@ -17,9 +17,36 @@ namespace Razor.Controllers
             Price = 275M
         };
 
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner Flag", Price = 34.95M}
+            };
+            return View(array);
+        }
+
         // GET: Home
         public ActionResult Index()
         {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpressions()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
             return View(myProduct);
         }
     }
