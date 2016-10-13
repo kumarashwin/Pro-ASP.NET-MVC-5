b4389dd268c69c8088ac2ea7d3299effbd787625
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-13 12:42:26 -0400

    Started on the SportsStore project in Chap. 7. Finished a basic scaffold.

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using SportsStore.Domain.Abstract;
+using Moq;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam) {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType) => kernel.TryGet(serviceType);
+        public IEnumerable<object> GetServices(Type serviceType) => kernel.GetAll(serviceType);
+
+        private void AddBindings()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns(new List<Product> {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Surfboard", Price = 179 },
+                    new Product { Name = "Running shoes", Price = 95}});
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+        }
+    }
+}
