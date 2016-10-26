8a0ea741547e411d9a20f4f37b22e93817cdb806
Ash Kumar <kumar.ashwin@outlook.com>
2016-10-26 14:13:58 -0400

    Finished Chapter. 14

---
+++ DebuggingDemo/DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace DebuggingDemo.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            int firstValue = 10;
+            int secondValue = 5;
+            int result = firstValue / secondValue;
+
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View(result);
+        }
+    }
+}
