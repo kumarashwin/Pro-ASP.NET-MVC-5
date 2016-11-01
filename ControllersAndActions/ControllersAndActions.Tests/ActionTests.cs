77628702bd2588545c1483971a5805d92d7e8979
Ash Kumar <kumar.ashwin@outlook.com>
2016-11-01 18:13:15 -0400

    Finished Chap. 17

---
+++ ControllersAndActions/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllersAndActions.Controllers;
+using System;
+
+namespace ControllersAndActions.Tests {
+
+    [TestClass]
+    public class ActionTests {
+
+        [TestMethod]
+        public void ControllerTest() {
+
+            // Arrange
+            var controller = new ExampleController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.AreEqual("Hello", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void RedirectTest() {
+
+            // Arrange
+            var controller = new ExampleController();
+
+            // Act
+            var result = controller.Redirect();
+
+            // Assert
+            Assert.IsFalse(result.Permanent);
+
+            Action<Action<string, string>, string, string> test = (func, a, b) => func(a,b);
+            test(Assert.AreEqual, "Example", (string)result.RouteValues["controller"]);
+            test(Assert.AreEqual, "Index", (string)result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void ViewSelectionTest() {
+            // Arrange
+            var controller = new ExampleController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsInstanceOfType(result.ViewData.Model, typeof(System.DateTime));
+        }
+    }
+}
---
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller {
+        public ViewResult Index() {
+            ViewBag.Title = "Index";
+            ViewBag.Message = TempData["Message"] ?? "Hello";
+            return View(model: DateTime.Now);
+        }
+
+        public RedirectToRouteResult Redirect() => RedirectToRoute(new { controller = "Example", action = "Index"});
+
+        public RedirectToRouteResult RedirectWithData(string message = "Default") {
+            TempData["Message"] = message;
+            return RedirectToAction("Index");
+        }
+    }
+}
