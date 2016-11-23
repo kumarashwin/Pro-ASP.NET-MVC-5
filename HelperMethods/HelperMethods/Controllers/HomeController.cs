03a6fbbd7b355c28c4f6a7a39e05fb49032e301a
Ash Kumar <kumar.ashwin@outlook.com>
2016-11-23 16:15:29 -0500

    Finished Chap. 21

---
+++ HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using HelperMethods.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers {
+    public class HomeController : Controller {
+
+        public ActionResult Index() {
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            return View(model: "This is an HTML element: <input>");
+        }
+
+        public ActionResult CreatePerson() => View(new Person());
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person) => View(person);
+    }
+}
---
+++ HelperMethods/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure {
+    public static class CustomHelpers {
+
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list) {
+            TagBuilder tag = new TagBuilder("ul");
+            foreach(string li in list) {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(li);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper html, string msg) =>
+            new MvcHtmlString("This is the message: <p>" + html.Encode(msg) + "</p>");
+    }
+}
