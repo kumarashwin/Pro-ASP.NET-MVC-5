e9f182caa0a05cdc606b4e217c01cedf33fad1ad
Ash Kumar <kumar.ashwin@outlook.com>
2016-11-29 14:50:49 -0500

    Finished Chap. 26

---
+++ ClientFeatures/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using ClientFeatures.Models;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers {
+    public class HomeController : Controller {
+        public ViewResult MakeBooking() => View(new Appointment { ClientName = "Adam", TermsAccepted = true });
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt) {
+            // Store new appointment in a repo/database
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ClientFeatures/ClientFeatures/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ClientFeatures {
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
